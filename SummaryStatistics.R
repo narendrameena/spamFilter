687cad65896b946af9b26718c3491418b693dd5b
Narendra Meena <narendrabhagri03@gmail.com>
2015-12-07 23:22:48 +0530

    text to csv

--- SummaryStatistics.R
+++ SummaryStatistics.R
@@ -3,20 +3,55 @@
 #author narendra
 
 
-#mean Function
+library(stringr) # to test functions 
+
+
+#read data from test file 
+data <- read.delim("test1.txt",header = TRUE)
+
+
+
+#finding minimum function
+my.min <- function(x) {
+  if(any(is.na(x))){     # remove NA or NAN from data
+    x<- x[-which(is.na(x)==TRUE)]
+  }
+  sortArray<-sort(x)
+  sortArray[1]
+}
+
+
+
+
+#finding maximum function
+my.max <- function(x) {
+  if(any(is.na(x))){     # remove NA or NAN from data
+    x<- x[-which(is.na(x)==TRUE)]
+  }
+  sortArray<-sort(x) # order list in increment order 
+  sortArray[length(sortArray)]
+}
+
+#finding mean function
 my.mean <- function(x) {
+  if(any(is.na(x))){     # to remove NA or NAN from data
+    x<- x[-which(is.na(x)==TRUE)]
+  }
   return(sum(x) / length(x))
 }
 
 
-#medain function
+#finding medain function
 my.median <- function(x) {
+  if(any(is.na(x))){     # to remove NA or NAN from data
+    x<- x[-which(is.na(x)==TRUE)]
+  }
   sorted.x <- sort(x)
   
   if (length(x) %% 2 == 0)
   {
     indices <- c(length(x) / 2, length(x) / 2 + 1)
-    return(mean(sorted.x[indices]))
+    return(my.mean(sorted.x[indices]))
   }
   else
   {
@@ -25,15 +60,59 @@ my.median <- function(x) {
   }
 }
 
-#varianc function
+#finding varianc function
 my.var <- function(x) {
-  m <- mean(x)
+  if(any(is.na(x))){      # to remove NA or NAN from data
+    x<- x[-which(is.na(x)==TRUE)]
+  }
+  m <- my.mean(x)
   return(sum((x - m) ^ 2) / (length(x) - 1))
 }
 
-#standrad deviation
+#finding standrad deviation
 my.sd <- function(x) {
+  if(any(is.na(x))){    # to remove NA or NAN from data
+    x<- x[-which(is.na(x)==TRUE)]
+  }
   return(sqrt(my.var(x)))
 }
 
+
+#testing custom functions
+
+#expect_equal(my.min(data$rna_act_3utr)-min(data$rna_act_3utr,na.rm = TRUE),0)
+#expect_equal(my.max(data$rna_act_3utr)-max(data$rna_act_3utr,na.rm = TRUE),0)
+#expect_equal(my.mean(data$rna_act_3utr)-mean(data$rna_act_3utr,na.rm = TRUE),0)
+#expect_equal(my.sd(data$rna_act_3utr)-sd(data$rna_act_3utr,na.rm = TRUE),0)
+#expect_equal(my.var(data$rna_act_3utr)-var(data$rna_act_3utr,na.rm = TRUE),0)
+#expect_equal(my.median(data$rna_act_3utr)-median(data$rna_act_3utr,na.rm = TRUE),0)
+
+
+
+#summary function with both custom functions and inbuild functions to compare
+sumStat <- function(x) {
+  if(is.numeric(x)){
+    c(min = min(x,na.rm = TRUE), my.min = my.min(x),
+      max = max(x,na.rm = TRUE), my.max = my.max(x),
+      mean = mean(x,na.rm = TRUE), my.mean = my.mean(x),
+      median = median(x,na.rm = TRUE), my.medain = my.median(x),
+      std = sd(x,na.rm = TRUE), my.sd = my.sd(x),
+      var=var(x,na.rm = TRUE), my.var = my.var(x))
+  }
+  else{
+    c(min = NA, my.min =NA,
+      max = NA, my.max= NA,
+      mean = NA, my.mean=NA,
+      median = NA, my.medain = NA,
+      std = NA, my.sd = NA,
+      var=NA, my.var =NA) 
+    
+  }
+}
+
+#stastical summary of data
+summary<- sapply(data, sumStat)
+summary
+#writing to csv file
+write.csv(rbind(data,summary), file="summaryDefault.csv")
 
--- emaiCorpus.R
+++ emaiCorpus.R
@@ -15,20 +15,27 @@ library(topicmodels)
 
 # get date of email
 get.date <- function(msg.vec) {
-  date.grep <- grepl("^Date: ", msg.vec)
+  date.grep <- grepl("^Date: |^Date:", msg.vec)
+  #print(length(date.grep))
   date.grepl <- which(date.grep == TRUE)
+  #print(date.grepl)
   date <- msg.vec[date.grep]
+  if(length(date)>0){
   date <- strsplit(date, "\\+|\\-|: ")[[1]][2]
+  #print(date)
   date <- gsub("^\\s+|\\s+$", "", date)
   #return(date)
   return(strtrim(date, 25))
+  }else{return('NA')}
 }
 
 # get subject of email
 get.subject <- function(msg.vec) {
   subj <- msg.vec[grepl("Subject: ", msg.vec)]
   if(length(subj) > 0) {
+ 
     return(strsplit(subj, "Subject: ")[[1]][2])
+    
     #return(subj)
   }
   else {
@@ -39,20 +46,27 @@ get.subject <- function(msg.vec) {
 #get the message of email
 get.msg <- function(msg.vec) {
   msg <- msg.vec[seq(which(msg.vec == "")[1] + 1, length(msg.vec), 1)]
+  #print(paste(msg, collapse="\n"))gsub("[\r\n]", "", x)
+  #return(paste(gsub("[\r\n]"," ",msg), collapse="\n"))
   return(paste(msg, collapse="\n"))
 }
 
 #get the sender email 
 get.from <- function(msg.vec) {
   from <- msg.vec[grepl("From: ", msg.vec)]
+  if(length(from)>0){
   from <- strsplit(from, '[":<> ]')[[1]]
+
   from <- from[which(from !="" & from !=" ")]
+  
   return(from[grepl("@", from)][1])
+  }else{return('NA')}
 }
          
 #fully message of email        
 msg.full <- function(path) {
-  con <- file(path, open="rt", encoding="latin1")
+  #con <- file(path, open="rt",encoding="latin1")
+  con <- file(path, open="rt",encoding="latin1")
   msg <- readLines(con)
   close(con)
   return(msg)
@@ -60,15 +74,59 @@ msg.full <- function(path) {
 
 parse.email <- function(path) {
   full.msg <- msg.full(path)
+  print(path)
   date <- get.date(full.msg)
   from <- get.from(full.msg)
   subj <- get.subject(full.msg)
   msg <- get.msg(full.msg)
+  print(msg)
   #return(c(date, from, subj, msg, path))
-  return(c(date,from, subj, msg, path, full.msg))
+  return(c(date,from, subj, msg, path))
 }
 
+setwd("/Users/naru/Documents/R_workshop/SpamFilter")
+email.files <- list.files("data", ".ivanova", recursive=TRUE, full.names=TRUE)
+#email.file <- list.files("data/", ".ivanova", recursive=TRUE, full.names=TRUE)[1]
+
+#codepages <- setNames(iconvlist(), iconvlist())
+
+#x <- lapply(codepages, function(enc) try(email.file))
 #email_txts
-email_txts[200]
+#email_txts[200]
 #parse.email(paste(data.path,email_txts[1]))
-parse.email("/Users/naru/Documents/R_workshop/SpamFilter/data/1423436694.12029_41.ivanova")
+result <- c(parse.email("/Users/naru/Documents/R_workshop/SpamFilter/data/1423436693.12029_23.ivanova"))
+write.csv(result, file = "Output.csv", append = T)
+#parse.email(email.file)
+
+for (i in email.files){
+  #print(i)
+  if(file.info(i)$size != 0){
+  result <- parse.email(i)
+  #print(paste(result[1],'\\t', result[2],'\\t', result[3],'\\t', result[5],'\\n'))
+  print(result)
+  #cat( result,file="output.csv",append=TRUE)
+  write.csv(result, file = "Output.csv", append = T)
+  #cat( "\n",file="output.csv",append=TRUE)
+  }
+  else{
+  print("File contain no data")
+    }
+}
+
+easyham.path <-"/Users/naru/Documents/R_workshop/SpamFilter/data/"
+easyham.docs <- dir(easyham.path)
+#easyham.docs <-"/Users/naru/Documents/R_workshop/SpamFilter/data/1424574180.8026_37.ivanova"
+easyham.docs <- easyham.docs[which(easyham.docs != "cmds")]
+#paste(easyham.path, p, sep="")
+parse.email("/Users/naru/Documents/R_workshop/SpamFilter/data/1424574180.8026_37.ivanova")
+easyham.parse <- lapply(easyham.docs, function(p) 
+{parse.email(paste(easyham.path, p, sep=""))})
+
+ehparse.matrix <- do.call(rbind, easyham.parse)
+allparse.df <- data.frame(ehparse.matrix, stringsAsFactors=FALSE)
+dim(allparse.df)
+allparse.df[5]
+names(allparse.df) <- c("Date", "From.EMail", "Subject", "Message", "Path")
+
+write.csv(allparse.df,file="spameDAta.csv")
+
